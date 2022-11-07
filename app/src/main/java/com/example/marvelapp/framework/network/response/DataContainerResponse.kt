a82a9afcfde8e860fe01ada61da285489923e99e
Ayrton Sato <ayrton_sato@hotmail.com>
2022-11-07 16:26:03 -0300

    Implementa CharactersPagingSource

--- app/src/main/java/com/example/marvelapp/framework/network/response/DataContainerResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/DataContainerResponse.kt
@@ -1,9 +1,9 @@
 package com.example.marvelapp.framework.network.response
 
 data class DataContainerResponse(
-    /*val offset: Int,
+    val offset: Int,
     val limit: Int,
     val total: Int,
-    val count: Int,*/
+    val count: Int,
     val results: List<CharacterResponse>
 )
